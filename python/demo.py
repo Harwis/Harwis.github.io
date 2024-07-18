23e3d1b0ff2a7fdd5b62b28e88fc3e7cc868f188
Harwis <harwis.liu@gmail.com>
2024-07-18 15:52:04 +0930

    Science 01

--- python/demo.py
+++ python/demo.py
@@ -1,63 +1,36 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Create an empty array of size 1000 x 1000 pixels
-width, height = 1000, 1000
-image_array = np.zeros((height, width, 3))
+# Create an image which have red and green pixels.
+width, height = 500, 500
+image_1 = np.zeros((height, width, 3))
 
-# Fill the array with a pattern of red and green pixels
 for y in range(height):
     for x in range(width):
         if (x + y) % 2 == 0:
-            image_array[y, x] = [1, 0, 0]  # Red
+            image_1[y, x] = [1, 0, 0]  # Red
         else:
-            image_array[y, x] = [0, 1, 0]  # Green
+            image_1[y, x] = [0, 1, 0]  # Green
 
-# Show the image
-plt.figure()
-plt.imshow(image_array, interpolation='none')
-
-
+# Create another image which looks has the similar colour as the first image
+image_2 = np.zeros((height, width, 3))
+image_2[:, :] = [184/255, 174/255, 40/255]
 
 
+# Show the images
+fig, ax = plt.subplots(1, 2)
+fig.set_size_inches(15, 7.5)
+ax[0].imshow(image_1, interpolation='none')
+ax[0].set_title('Image 1')
+ax[1].imshow(image_2, interpolation='none')
+ax[1].set_title('Image 2')
+plt.show()
 
 
 
 
 
-# # RGB
-# from PIL import Image
-#
-# # Define the size of the image
-# width, height = 1000, 1000
-#
-# # Create a new image with RGB mode
-# image = Image.new('RGB', (width, height))
-#
-# # Get the pixel map of the image
-# pixels = image.load()
-#
-# # Define colors
-# green = (0, 255, 0)
-# red = (255, 0, 0)
-# blue = (0, 0, 255)
-#
-# # Fill the image with the pattern
-# for y in range(height):
-#     for x in range(width):
-#         if (x % 2 == 1 and y % 2 == 1):
-#             pixels[x, y] = red  # Red pixel
-#         elif (x % 2 == 0 and y % 2 == 0):
-#             pixels[x, y] = blue  # Blue pixel
-#         else:
-#             pixels[x, y] = green  # Green pixel
-#
-#
-#
-#
-# # Save the image
-# image.save('ccd_pattern_1000x1000.png')
-#
-# # Display the image
-# image.show()
+
+
+
 
